d3a323cca40725074b38ebed4aa6f268288ce88a
Zac Driscoll <zgdriscoll@gmail.com>
2020-10-04 17:35:07 -0500

    finalized tests for format_spatial_coords functions

--- tests/testthat/test_format_spatial_coords.R
+++ tests/testthat/test_format_spatial_coords.R
@@ -2,73 +2,68 @@ context("Formatting Coordinates")
 library(arcgis.rest)
 
 #create spatial and character objects to run tests on
-#create point object to test
-formatted_point <- cbind(0,1)%>%
-  sf::st_point() %>%
-  sf::st_sfc() %>%
-  sf::st_sf(crs = 4326)
-
-formatted_point_char <- "TEST"
-
 #create multipoint object to test
-formatted_multipoint <- rbind(c(0, 0), c(1, 1), c(2, 1)) %>%
+multipoint <- rbind(c(0, 0), c(1, 1), c(2, 1)) %>%
   sf::st_multipoint() %>%
   sf::st_sfc() %>%
   sf::st_sf(crs = 4326)
-
-formatted_multipoint_char <-
-  paste("{'points':[[[0,0],[1,1],[2,1]]],",
-        "'spatialReference':{'wkid':4326}}",
-        sep = "")
-
 #create line object to test
-formatted_line <- rbind(c(0, 0), c(1, 1), c(2, 1)) %>%
+line <- rbind(c(0, 0), c(1, 1), c(2, 1)) %>%
   sf::st_linestring() %>%
   sf::st_sfc() %>%
   sf::st_sf(crs = 4326)
-
-formatted_line_char <-
-  paste("{'paths':[[[0,0],[1,1],[2,1]]],",
-        "'spatialReference':{'wkid':4326}}",
-        sep = "")
-
 #create polygon object to test
-formatted_poly <- list(rbind(c(0, 0), c(0, 1), c(1, 1), c(1, 0), c(0, 0))) %>%
+poly <-
+  list(rbind(
+    c(0, 0), c(0, 1), c(0, 2), c(1, 2), c(1, 3), c(2, 3),
+    c(2, 4), c(2, 5), c(1, 5), c(0, 5), c(0, 3), c(0, 0)
+  )) %>%
   sf::st_polygon() %>%
   sf::st_sfc() %>%
   sf::st_sf(crs = 4326)
 
-formatted_poly_char <-
-  paste("{'rings':[[[0,0],[0,1],[1,1],[1,0],[0,0]]],",
+#run tests
+#test format_multipoint_coords
+expected_mulitpoint_output <-
+  paste("{'points':[[[0,0],[1,1],[2,1]]],",
         "'spatialReference':{'wkid':4326}}",
         sep = "")
 
-#Run Tests
-#test format_point_coords
-# test_that("format_point_coords returns a properly formatted character object",
-#           {
-#             test_formatted_point <- format_point_coords(formatted_point)
-#             expect_equal(formatted_point_char, test_formatted_point)
-#           })
-
-#test format_multipoint_coords
 test_that("format_multipoint_coords returns a properly formatted character object",
           {
-            test_formatted_multipoint <-
-              format_multipoint_coords(formatted_multipoint)
-            expect_equal(formatted_multipoint_char, test_formatted_multipoint)
+            actual_multipoint_output <-
+              format_multipoint_coords(multipoint)
+            expect_equal(expected_mulitpoint_output, actual_multipoint_output)
           })
 
 #test format_line_coords
+expected_line_output <-
+  paste("{'paths':[[[0,0],[1,1],[2,1]]],",
+        "'spatialReference':{'wkid':4326}}",
+        sep = "")
+
 test_that("format_line_coords returns a properly formatted character object",
           {
-            test_formatted_line <- format_line_coords(formatted_line)
-            expect_equal(formatted_line_char, test_formatted_line)
+            actual_line_output <- format_line_coords(line)
+            expect_equal(expected_line_output, actual_line_output)
           })
 
 #test format_polygon_coords
+expected_polygon_output <-
+  paste("{'rings':[[[0,0],[0,1],[0,2],[1,2],[1,3],[2,3],[2,4],[2,5],[1,5],",
+        "[0,5],[0,3],[0,0]]],'spatialReference':{'wkid':4326}}",
+        sep = "")
+
 test_that("format_polygon_coords returns a properly formatted character object",
           {
-            test_formatted_poly <- format_polygon_coords(mke_county)
-            expect_equal(formatted_poly_char, test_formatted_poly)
+            actual_polygon_output <- format_polygon_coords(poly)
+            expect_equal(expected_polygon_output, actual_polygon_output)
             })
+#test format_envelope_coords
+expected_envelope_output <- "xmin : 0, ymin : 0, xmax : 2, ymax : 5"
+
+test_that("format_envelope_coords returns a properly formatted character object",
+          {
+            actual_envelope_output <- format_envelope_coords(poly)
+            expect_equal(expected_envelope_output, actual_envelope_output)
+          })
