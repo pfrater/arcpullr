f9c41b591b401378753872dedd4786cb1edc584f
Paul Frater <paul.frater@wisconsin.gov>
2025-07-21 11:20:01 -0500

    Added county code to wis_county sf data.frame

---
+++ data-raw/wis_polygons.R
@@ -0,0 +1,31 @@
+devtools::load_all(".")
+
+wis_poly <-
+  maps::map("state", regions = "wisconsin",
+            plot = FALSE, fill = TRUE, bg = NA) |>
+  sf::st_as_sf()
+
+wis_counties <-
+  maps::map("county", regions = "wisconsin",
+            plot = FALSE, fill = TRUE, bg = NA) |>
+  sf::st_as_sf() |>
+  tidyr::separate(ID, into = c("state", "county"), sep = ",") |>
+  dplyr::arrange(county) |>
+  dplyr::mutate(county = tolower(gsub(" ", "_", county)), county.code = 1:72) |>
+  dplyr::relocate(county.code, .after = county)
+
+mke_county <-
+  wis_counties |>
+  dplyr::filter(county == "milwaukee")
+portage_county <-
+  wis_counties |>
+  dplyr::filter(county == "portage")
+
+
+usethis::use_data(
+  wis_poly,
+  wis_counties,
+  mke_county,
+  portage_county,
+  overwrite = TRUE
+)
